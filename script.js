04e569fa90a703afa895ef969374c64140a05638
Cao ​​ <dochicao2306@gmail.com>
2023-02-21 21:07:45 +0700

    v4.3.1

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() 
+{
+    var loading = document.getElementById('lds-grid');
+    loading.style.display = 'none';
+});
+  
+document.addEventListener('DOMContentLoaded', function() 
+{
+    var loading = document.getElementById('lds-grid');
+    loading.style.display = 'block';
+});
