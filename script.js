3320abd72279e4f44653f547eb4bdb925934675d
Cao ​​ <dochicao2306@gmail.com>
2023-02-23 14:39:16 +0700

    v4.3.4

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-window.addEventListener('load', function () 
+document.addEventListener("DOMContentLoaded", function(event) 
 {
-    document.getElementById('loading').style.display = 'none';
-});  
+    document.getElementById("loading").style.display = "flex";
+});
+
+setTimeout(function() 
+{
+    document.getElementById("loading").style.display = "none";
+}, 3000);
